b118fd2d883bc15480adb95c256dbee7128da167
dhruv-karan <dhruvkaran24@gmail.com>
2019-03-05 19:54:10 +0530

    week 3 passed

--- week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
+++ week3_greedy_algorithms/2_maximum_value_of_the_loot/fractional_knapsack.py
@@ -2,23 +2,22 @@
 import sys
 def get_optimal_value(capacity, weights, values):
     value = 0
-    weights = [weights.sort()]
-    type(weights)
-    values = [values.sort()]
     ratio =  [x/y for x,y in zip(values,weights)]
-    inx ,max_val = ratio.index(max(ratio)) , max(ratio)
-    if (capacity > weights[inx]):
-        value += values[inx]
-        capacity -= weights[inx]
-        del weights[inx]
-        del ratio[inx]
-        del capacity[inx]
-    elif(capacity ==0):
-        return value
-    elif(capacity < weights[inx]):
-        value += max_val*capacity
-        return value
-    
+    while len(ratio)>0:
+        inx ,max_val = ratio.index(max(ratio)) , max(ratio)
+        if (capacity > weights[inx]):
+            value += values[inx]
+            capacity -= weights[inx]
+            del weights[inx]
+            del ratio[inx]
+            del values[inx]
+        elif(capacity ==0):
+            return value
+        elif(capacity <= weights[inx]):
+            value += max_val*capacity
+            return value
+        else:
+            return value
     return value
 
 
@@ -29,12 +28,3 @@ if __name__ == "__main__":
     weights = data[3:(2 * n + 2):2]
     opt_value = get_optimal_value(capacity, weights, values)
     print("{:.10f}".format(opt_value))
-
-
-print(get_optimal_value(20,[5,7,3,8,1,4],[300,200,176,98,65,500]))
-
-
-weights=[5,7,3,8,1,4]
-
-weights = [weights.sort()]
-print(type(weights))
